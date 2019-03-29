b88d06bf98de2765ef9852a3e87c0a69b04b398e
Xupp <Xupp2015@163.com>
2019-03-29 14:47:54 +0800

    添加ioEntity

---
+++ src/main/java/com/example/demo/ioEntity/Student.java
@@ -0,0 +1,37 @@
+package com.example.demo.ioEntity;
+
+import java.io.Serializable;
+
+/**
+ *
+ */
+public class Student implements Serializable {
+    private static final long serialVersionUID = 918185028445359909L;
+    private String name;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Student{");
+        sb.append("name='").append(name).append('\'');
+        sb.append(", age='").append(age).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
